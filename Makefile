bf3ce7a0e076f9bbe6abbc4bb49d100ca9c6f092
Gabriel Florit <gabrielflorit@gmail.com>
2015-02-26 16:12:54 -0500

    poc

--- Makefile
+++ Makefile
@@ -8,3 +8,7 @@ R_deploy:
 	cp data/03-01-2015-boom.html /Volumes/www_html/multimedia/graphics/projectFiles/Rmd/
 	rsync -rv data/03-01-2015-boom_files /Volumes/www_html/multimedia/graphics/projectFiles/Rmd
 	open http://private.boston.com/multimedia/graphics/projectFiles/Rmd/03-01-2015-boom.html
+
+prepare:
+
+	csvjson ${file} > data/data.json
--- graphics/graphic/js/main.js
+++ graphics/graphic/js/main.js
@@ -1,3 +1,37 @@
-// Put all your code here.
-// NOTE: no need to do $(document).ready, or $(function() { etc here.
-// Just start writing js.
+var master = $('.igraphic-graphic.graphic');
+
+function toNumber(s) {
+
+	var result = s && s.length ?
+		+(s.replace(/\$/g, '').replace(/,/g, '')) :
+		null;
+
+	return result;
+}
+
+var data = _.chain(require('../../../data/data.json'))
+	.map(function(v, i) {
+		v.Cost = toNumber(v.Cost);
+		v['Square feet'] = toNumber(v['Square feet']);
+
+		if (v.Longitude && v.Longitude.length && v.Latitude && v.Latitude.length) {
+			v.mapUrl = 'http://api.tiles.mapbox.com/v4/' + 'gabriel-florit.e222ba6f' + '/pin-l-commercial+482(' + v.Longitude + ',' + v.Latitude + ')/' + v.Longitude + ',' + v.Latitude + ',15/500x300.png?access_token=pk.eyJ1IjoiZ2FicmllbC1mbG9yaXQiLCJhIjoiVldqX21RVSJ9.Udl7GDHMsMh8EcMpxIr2gA';
+		}
+
+		return v;
+	})
+	.sortBy('Square feet')
+	.reverse()
+	.value();
+
+function log(data) {
+	console.log(JSON.stringify(data, null, 4));
+}
+
+log(data);
+
+
+
+$('.content', master).html(_.templates.buildings({
+	buildings: data
+}));
