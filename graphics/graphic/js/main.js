5ddd875c92f5c2ec4038a586de0fb7e8a1132c78
Gabriel Florit <gabrielflorit@gmail.com>
2015-02-26 18:03:08 -0500

    more

--- graphics/graphic/js/main.js
+++ graphics/graphic/js/main.js
@@ -1,4 +1,8 @@
 var master = $('.igraphic-graphic.graphic');
+var Masonry = require('masonry');
+var imagesLoaded = require('imagesloaded');
+
+// debugger;
 
 function toNumber(s) {
 
@@ -15,7 +19,8 @@ var data = _.chain(require('../../../data/data.json'))
 		v['Square feet'] = toNumber(v['Square feet']);
 
 		if (v.Longitude && v.Longitude.length && v.Latitude && v.Latitude.length) {
-			v.mapUrl = 'http://api.tiles.mapbox.com/v4/' + 'gabriel-florit.e222ba6f' + '/pin-l-commercial+482(' + v.Longitude + ',' + v.Latitude + ')/' + v.Longitude + ',' + v.Latitude + ',15/500x300.png?access_token=pk.eyJ1IjoiZ2FicmllbC1mbG9yaXQiLCJhIjoiVldqX21RVSJ9.Udl7GDHMsMh8EcMpxIr2gA';
+			v.mapUrl = '../../locator.png';
+			// v.mapUrl = 'http://api.tiles.mapbox.com/v4/' + 'gabriel-florit.e222ba6f' + '/pin-l-commercial+482(' + v.Longitude + ',' + v.Latitude + ')/' + v.Longitude + ',' + v.Latitude + ',15/500x300.png?access_token=pk.eyJ1IjoiZ2FicmllbC1mbG9yaXQiLCJhIjoiVldqX21RVSJ9.Udl7GDHMsMh8EcMpxIr2gA';
 		}
 
 		return v;
@@ -32,6 +37,15 @@ log(data);
 
 
 
-$('.content', master).html(_.templates.buildings({
-	buildings: data
-}));
+$('.buildings', master)
+	.html(_.templates.buildings({
+		buildings: data
+	}))
+	.imagesLoaded(function() {
+		
+		var msnry = new Masonry(document.querySelector('.igraphic-graphic.graphic .buildings'), {
+			itemSelector: '.building',
+			columnWidth: '.sizer',
+			gutter: '.gutter'
+		});
+	});
