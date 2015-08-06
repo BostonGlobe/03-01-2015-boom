50071d4c248844fc3d1cabc777b842fdbf9e714b
Gabriel Florit <gabrielflorit@gmail.com>
2015-08-06 13:12:32 -0400

    add file

---
+++ data/makeDimensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+images=data/image*.png
+
+echo 'filename, width, height';
+
+for i in $images
+do
+	dimensions=$(identify -format '%w, %h' $i);
+	echo $i', '$dimensions;
+done
--- index.js
+++ index.js
@@ -18,6 +18,7 @@ fs.writeFileSync('data/downloadMaps.sh', json.filter(function(v) {
 
 	var lonlat = [v.Longitude, v.Latitude].join(',');
 
+
 	var url = 'http://api.tiles.mapbox.com/v4/gabriel-florit.e222ba6f/pin-l-commercial+464646(' + lonlat + ')/' + lonlat + ',14/300x185.png?access_token=pk.eyJ1IjoiZ2FicmllbC1mbG9yaXQiLCJhIjoiVldqX21RVSJ9.Udl7GDHMsMh8EcMpxIr2gA';
 	var filename = 'locatormap_' + v.line_number + '.png';
 	var command = 'curl "' + url + '" > data/locatormap_' + v.line_number + '.png;';
@@ -42,42 +43,3 @@ fs.writeFileSync('data/downloadPhotos.sh', json.filter(hasImage).map(function(v)
 	return command;
 
 }).join('\n'));
-
-
-
-// make script to make dimensions.json
-// we somehow have to gernate a csv or json of width,height,line_number for json datums with hasImage=true
-
-
-
-// var dimensions = json.filter(function(v) {
-
-
-// }).map(function(v) {
-
-// 	console.log(JSON.stringify(command, null, 4));
-// 	shelljs.exec(command, function (code, output) {
-// 		console.log(JSON.stringify(code, null, 4));
-// 		console.log(JSON.stringify(output, null, 4));
-// 	});
-
-// 	return v;
-
-// }).map(function(v) {
-
-// 	try {
-// 		var result = sizeOf('data/image_' + v.line_number + '.png');
-// 		result.line_number = v.line_number;
-// 		console.log(JSON.stringify(result, null, 4));
-
-// 		return result;
-// 	} catch(e) {
-
-// 		console.log(e);
-// 		console.log(JSON.stringify(v, null, 4));
-
-// 	}
-
-// });
-
-// fs.writeFileSync('data/dimensions.json', JSON.stringify(dimensions, null, 4));
